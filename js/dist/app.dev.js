6bc7b5e4301c6ac495722c482a49027d066b537d
Nodirbek <nbek.3d@gmail.com>
2022-11-20 05:28:46 +0500

    swipper

--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -31,3 +31,23 @@ window.onload = function () {
     document.querySelector('.header-2').classList.remove('active');
   }
 };
+
+var swiper = new Swiper(".book-slider", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 950,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    }
+  }
+});
