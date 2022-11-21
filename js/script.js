ce3a70d8203affd24e337e3aaff6c76b409c9dae
Nodirbek <nbek.3d@gmail.com>
2022-11-21 05:36:09 +0500

    ready

--- js/script.js
+++ js/script.js
@@ -1,154 +1,160 @@
-"use strict";
-
 searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = function () {
+document.querySelector('#search-btn').onclick = () =>{
   searchForm.classList.toggle('active');
-};
+}
 
-var loginForm = document.querySelector('.login-form-container');
+let loginForm = document.querySelector('.login-form-container');
 
-document.querySelector('#login-btn').onclick = function () {
+document.querySelector('#login-btn').onclick = () =>{
   loginForm.classList.toggle('active');
-};
+}
 
-document.querySelector('#close-login-btn').onclick = function () {
+document.querySelector('#close-login-btn').onclick = () =>{
   loginForm.classList.remove('active');
-};
+}
 
-window.onscroll = function () {
+window.onscroll = () =>{
+
   searchForm.classList.remove('active');
 
-  if (window.scrollY > 80) {
+  if(window.scrollY > 80){
     document.querySelector('.header .header-2').classList.add('active');
-  } else {
+  }else{
     document.querySelector('.header .header-2').classList.remove('active');
   }
-};
 
-window.onload = function () {
-  if (window.scrollY > 80) {
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
     document.querySelector('.header .header-2').classList.add('active');
-  } else {
+  }else{
     document.querySelector('.header .header-2').classList.remove('active');
   }
 
   fadeOut();
-};
 
-function loader() {
+}
+
+function loader(){
   document.querySelector('.loader-container').classList.add('active');
 }
 
-function fadeOut() {
+function fadeOut(){
   setTimeout(loader, 4000);
 }
 
 var swiper = new Swiper(".books-slider", {
-  loop: true,
+  loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
 });
+
 var swiper = new Swiper(".featured-slider", {
   spaceBetween: 10,
-  loop: true,
+  loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
+    prevEl: ".swiper-button-prev",
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     450: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     1024: {
-      slidesPerView: 4
-    }
-  }
+      slidesPerView: 4,
+    },
+  },
 });
+
 var swiper = new Swiper(".arrivals-slider", {
   spaceBetween: 10,
-  loop: true,
+  loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
 });
+
 var swiper = new Swiper(".reviews-slider", {
   spaceBetween: 10,
-  grabCursor: true,
-  loop: true,
+  grabCursor:true,
+  loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
 });
+
 var swiper = new Swiper(".blogs-slider", {
   spaceBetween: 10,
-  grabCursor: true,
-  loop: true,
+  grabCursor:true,
+  loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
-    disableOnInteraction: false
+    disableOnInteraction: false,
   },
   breakpoints: {
     0: {
-      slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-      slidesPerView: 3
-    }
-  }
+      slidesPerView: 3,
+    },
+  },
 });
